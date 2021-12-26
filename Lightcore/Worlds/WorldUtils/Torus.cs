19c9d79a13705200c3931c6fff6dabe34f6b355b
Kristoffer Kjeldby <Kristoffer.Kjeldby@isobar.com>
2021-12-26 20:18:52 +0100

    Optimize torus

--- Lightcore/Worlds/WorldUtils/Torus.cs
+++ Lightcore/Worlds/WorldUtils/Torus.cs
@@ -4,21 +4,22 @@
     using Lightcore.Common.Cartesian.Extensions;
     using Lightcore.Common.Models;
     using Lightcore.Textures;
+    using Lightcore.Worlds.Extensions;
     using System;
-    using System.Collections.Generic;
 
     public partial class WorldUtils
     {
         public static Entity Torus(EntityType entityType, Vector origon, double radius1, double radius2, Tuple<double, Vector>[,] map)
         {
-            var polygons = new List<Polygon>();
+            var polygons = new Polygon[map.Size() * 2];
+            int p = 0;
 
             var cStepSize = Constants.PI2 / map.GetLength(0);
-            var pStepSize = Constants.PI2 / map.GetLength(1); 
+            var pStepSize = Constants.PI2 / map.GetLength(1);
 
             for (int c0 = 0; c0 < map.GetLength(0); c0++)
             {
-                var c1 = (c0 == map.GetLength(0) - 1) ? 0 :  c0 + 1;
+                var c1 = (c0 == map.GetLength(0) - 1) ? 0 : c0 + 1;
 
                 var cc0 = new Vector(Math.Cos(c0 * cStepSize), Math.Sin(c0 * cStepSize), 0);
                 var cc1 = new Vector(Math.Cos(c1 * cStepSize), Math.Sin(c1 * cStepSize), 0);
@@ -41,8 +42,7 @@
                     var vectorcc0pp1 = (cc0 * radius1) + cc0pp1 + (cc0pp1.Unit() * map[c0, p1].Item1);
                     var vectorcc1pp0 = (cc1 * radius1) + cc1pp0 + (cc1pp0.Unit() * map[c1, p0].Item1);
 
-                    polygons.Add
-                    (
+                    polygons[p++] =
                         new Polygon
                         (
                             ColorTextureStore.Get(map[c0, p0].Item2),
@@ -52,11 +52,9 @@
                                 vectorcc1pp1,
                                 vectorcc0pp1
                             }
-                        )
-                    );
+                        );
 
-                    polygons.Add
-                    (
+                    polygons[p++] =
                         new Polygon
                         (
                             ColorTextureStore.Get(map[c0, p0].Item2),
@@ -66,15 +64,14 @@
                                 vectorcc0pp0,
                                 vectorcc1pp0
                             }
-                        )
-                    );
+                        );
 
                     cc0pp0 = cc0pp1;
                     cc1pp0 = cc1pp1;
                 }
             }
 
-            return new Entity(entityType, polygons.ToArray());
+            return new Entity(entityType, polygons);
         }
     }
 }
